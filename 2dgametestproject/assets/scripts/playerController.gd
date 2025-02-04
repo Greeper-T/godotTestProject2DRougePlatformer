e5521402ea44c7d7533f27c66e7d2457b47b0cb3
Greeper-T <hyrtwisback@gmail.com>
2025-02-04 09:06:15 -0600

    gun partial

--- 2dgametestproject/assets/scripts/playerController.gd
+++ 2dgametestproject/assets/scripts/playerController.gd
@@ -50,5 +50,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * speed * speedMultiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, speedMultiplier * speed)
+		
+	
 
 	move_and_slide()
