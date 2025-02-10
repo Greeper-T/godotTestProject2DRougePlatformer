e866fb12951455c263adf3118f66de2efb23319b
Cjb-333 <cjbadillo333@gmail.com>
2025-02-10 09:05:26 -0600

    Potion count

--- 2dgametestproject/assets/scripts/gameManager.gd
+++ 2dgametestproject/assets/scripts/gameManager.gd
@@ -4,23 +4,27 @@ var currentArea = 1
 var areaPath = "res://assets/scenes/playable/"
 
 var potions = 0
-var area_container : Node2D
-var player : PlayerController
+#var area_container : Node2D
+#var player : PlayerController
 var hud : HUD
 
 
 func _ready():
+	var hud_nodes = get_tree().get_nodes_in_group("hud")
+	print("HUD nodes found:", hud_nodes)
 	hud = get_tree().get_first_node_in_group("hud")
-	area_container = get_tree().get_first_node_in_group("area_container")
-	player = get_tree().get_first_node_in_group("player")
-	reset_potions()
 
+#func _initialize_hud():
+	#hud = get_tree().get_first_node_in_group("hud")
+	
 
 func nextLevel():
 	currentArea += 1
 	var fullPath = areaPath + "area_" + str(currentArea) + ".tscn"
 	get_tree().change_scene_to_file(fullPath)
-	print("entered portal")
+	print_debug("entered portal")
+	print(hud)
+	
 
 
 
@@ -29,11 +33,13 @@ func set_up_area():
 
 
 func add_potion():
+	hud = get_tree().get_first_node_in_group("hud")
 	potions += 1
 	hud.update_potion_label(potions)
-
+ 
 
 func reset_potions():
 	potions = 0
+	print(potions)
 	hud.update_potion_label(potions)
 	
--- 2dgametestproject/assets/scripts/hud.gd
+++ 2dgametestproject/assets/scripts/hud.gd
@@ -2,6 +2,12 @@ extends Control
 class_name HUD
 
 @export var potion_label : Label
+@export var item_label : Label
+
+
+
+
+
 
 func update_potion_label(number : int):
 	potion_label.text = "x " + str(number)
