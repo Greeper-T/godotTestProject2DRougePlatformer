6c80292d4231145578f9f825a8310e703392fcf2
Cjb-333 <cjbadillo333@gmail.com>
2025-03-10 09:04:44 -0500

    popup wip

--- 2dgametestproject/assets/GUI/slot.gd
+++ 2dgametestproject/assets/GUI/slot.gd
@@ -1,13 +1,17 @@
 extends PanelContainer
 
 
+# Helper function to get the Popups node from the group
+func get_popups():
+	var popups = get_tree().get_nodes_in_group("Popups")
+	print("Found Popups nodes:", popups) # Debugging
+	return null
 
 
 func _on_mouse_entered() -> void:
-	Popups.ItemPopup(null, null)
-
-
-
+	var popups = get_tree().get_first_node_in_group("Popups")
+	popups.ItemPopup(null, null)
 
 func _on_mouse_exited() -> void:
-	pass # Replace with function body.
+	var popups = get_tree().get_first_node_in_group("Popups")
+	popups.HideItemPopup()
