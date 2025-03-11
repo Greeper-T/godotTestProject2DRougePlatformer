c4cfae92187247ada5fce081b8ca29c6f8d0b58b
Cjb-333 <cjbadillo333@gmail.com>
2025-03-11 10:15:03 -0500

    pop

--- 2dgametestproject/assets/GUI/slot.gd
+++ 2dgametestproject/assets/GUI/slot.gd
@@ -1,17 +1,8 @@
 extends PanelContainer
 
 
-# Helper function to get the Popups node from the group
-func get_popups():
-	var popups = get_tree().get_nodes_in_group("Popups")
-	print("Found Popups nodes:", popups) # Debugging
-	return null
-
-
 func _on_mouse_entered() -> void:
-	var popups = get_tree().get_first_node_in_group("Popups")
-	popups.ItemPopup(null, null)
+	Popups.ItemPopup(Rect2i( Vector2i(global_position), Vector2i(size)), null)
 
 func _on_mouse_exited() -> void:
-	var popups = get_tree().get_first_node_in_group("Popups")
-	popups.HideItemPopup()
+	Popups.HideItemPopup()
--- 2dgametestproject/assets/scripts/itemScripts/Item.gd
+++ 2dgametestproject/assets/scripts/itemScripts/Item.gd
@@ -1,16 +1,20 @@
-class_name ItemData
 extends Resource
+class_name Item
 
 enum Type {COMMON, UNCOMMON, RARE, LEGENDARY, BOSS, CHARACTER, MAIN}
 
+@export_category("Information")
 @export var type: Type
 @export var itemName: String
 @export var itemId: int
+@export var itemTexture: Texture2D
+@export_multiline var itemDescription: String
+
+@export_category("Stats")
 @export var itemDamage: int
 @export var itemDefense: int
 @export var itemCritAdd: int
 @export var itemCritMult: int
 @export var itemHealthIncrease: int
+@export var itemSpeedIncrease: int
 @export var itemAbility: String
-@export_multiline var itemDescription: String
-@export var itemTexture: Texture2D
