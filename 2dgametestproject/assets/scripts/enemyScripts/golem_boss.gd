44c6223f0ca491b33c8670f4ea1890902fb3ded8
Greeper-T <hyrtwisback@gmail.com>
2025-03-13 09:06:25 -0500

    more boss stuff

--- 2dgametestproject/assets/scripts/enemyScripts/golem_boss.gd
+++ 2dgametestproject/assets/scripts/enemyScripts/golem_boss.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @onready var player = get_parent().find_child("player")
 @onready var debug: Label = $debug
 @onready var muzzle: Node2D = $bossTexure/muzzle
+@onready var pivot: Node2D = $pivot
+@onready var lazer_eye: AnimatedSprite2D = $pivot/lazerEye
 
 @export var bullet: PackedScene
 
@@ -16,6 +18,7 @@ var direction : Vector2
 
 func _ready() -> void:
 	set_physics_process(false)
+	lazer_eye.stop()
 
 func _physics_process(delta: float) -> void:
 	checkForMelee()
@@ -50,6 +53,10 @@ func dash():
 	tween.tween_property(self ,"position", player.global_position, 0.8)
 	await tween.finished
 
+func setTarget():
+	var temp = (player.global_position - pivot.global_position).normalized()
+	pivot.rotation = temp.angle()
+
 func changeState(newState):
 	if newState != currentState:
 		previousState = currentState
@@ -58,7 +65,7 @@ func changeState(newState):
 		debug.text = State.keys()[currentState]
 
 func playAnimations():
-	if currentState == State.Idle:
+	if currentState == State.Idle or currentState == State.Follow:
 		boss_texure.play("idle")
 	elif currentState == State.MeleeAttack:
 		boss_texure.play("meleeAttack")
@@ -71,6 +78,14 @@ func playAnimations():
 		boss_texure.play("glowing")
 		await dash()
 		changeState(State.Follow)
+	elif currentState == State.LaserBeam:
+		boss_texure.play("lazarCast")
+		await boss_texure.animation_finished
+		lazer_eye.play("charge")
+		setTarget()
+		await lazer_eye.animation_finished
+		lazer_eye.stop()
+		changeState(State.Dash)
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	set_physics_process(true)
@@ -79,5 +94,9 @@ func _on_player_detector_body_entered(body: Node2D) -> void:
 
 func _on_player_detector_body_exited(body: Node2D) -> void:
 	set_physics_process(false)
-	changeState(State.HomingMissile)
-	
+	var chance = randi() % 2
+	match chance:
+		0:
+			changeState(State.HomingMissile)
+		1:
+			changeState(State.LaserBeam)
