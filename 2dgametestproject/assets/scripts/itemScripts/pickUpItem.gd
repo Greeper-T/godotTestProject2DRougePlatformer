a43f6732f36bfe8898ccfe22d72240249806b711
Cjb-333 <cjbadillo333@gmail.com>
2025-04-01 10:06:11 -0500

    yes

--- 2dgametestproject/assets/scripts/itemScripts/pickUpItem.gd
+++ 2dgametestproject/assets/scripts/itemScripts/pickUpItem.gd
@@ -1,14 +1,14 @@
-exextends Area2D
+extends Area2D
 
-@export var item: Item  # Assign a random item when spawned
-@onready var sprite: Sprite2D = $Sprite2D  # Reference to sprite component
+@export var item: Item  # Assign an item resource in the editor
+@onready var sprite: Sprite2D = $Sprite2D  # Reference to the sprite
 
 func _ready():
 	if item:
-		sprite.texture = item.texture  # Display item texture
+		sprite.texture = item.texture  # Display correct item texture
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerController:
 		var added = GameManager.add_item_to_inventory(item)
 		if added:
-			queue_free()  # Remove the item after pickup
+			queue_free()  # Remove item after pickup
