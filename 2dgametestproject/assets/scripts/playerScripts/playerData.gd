a6ce84c8111829c64ae552414c7f462f28449c02
Greeper-T <hyrtwisback@gmail.com>
2025-04-07 08:32:46 -0500

    golem player finsihed

--- 2dgametestproject/assets/scripts/playerScripts/playerData.gd
+++ 2dgametestproject/assets/scripts/playerScripts/playerData.gd
@@ -15,9 +15,12 @@ var currentState = PlayerState.IDLE
 
 func takeDamage(amount: float):
 	hp = max(hp - amount, 0)  # Prevents negative HP
-	if hp == 0:
+	
+
+func die():
+	if hp <= 0:
 		get_tree().change_scene_to_file("res://assets/scenes/areaFunctions/start_menu.tscn")
-		GameManager.currentArea = 1  
+		GameManager.currentArea = 1
 
 func addHp(amount: float):
 	if GameManager.usePotion():
